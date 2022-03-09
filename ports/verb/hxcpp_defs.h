b5d1efb173b4a8924883149facc282255b654387
maz-1 <ohmygod19993@gmail.com>
2022-03-09 15:31:11 +0800

    [verb] add port

---
+++ ports/verb/hxcpp_defs.h
@@ -0,0 +1,37 @@
+#pragma once
+#ifdef WIN32
+	#define HX_WINDOWS
+	#ifdef _WIN64
+		#define HXCPP_M64
+	#endif
+#endif
+#ifdef NDEBUG
+	#define NO_HXCPP_DEBUG
+#else
+#endif
+#include <HxcppConfig.h>
+#ifndef HXCPP_H
+#include <hxcpp.h>
+#endif
+
+
+static inline int HaxeInitGC()
+{
+	HX_TOP_OF_STACK
+    hx::Boot();
+    try
+    {
+         __boot_all();
+    }
+    catch (Dynamic e)
+    {
+         __hx_dump_stack();
+         #ifdef HX_WIN_MAIN
+         MessageBoxA(0,  e==null() ? "null" : e->toString().__CStr(), "Error", 0);
+         #else
+         printf("Error : %s\n",e==null() ? "null" : e->toString().__CStr());
+         #endif
+         return -1;
+    }
+    return 0;
+}
